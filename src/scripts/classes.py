0c54fcc30dacc967eee643e071cf730cc7298a40
Jan Koca <kocajan@fel.cvut.cz>
2023-03-12 01:05:45 +0100

    major improvement in detection

--- src/scripts/classes.py
+++ src/scripts/classes.py
@@ -1,10 +1,161 @@
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+from matplotlib.patches import Rectangle
+
+
+class Map:
+    def __init__(self, dimensions):
+        self.dimensions = dimensions
+        self.garage = None
+        self.obstacles = []
+        self.robot = None
+
+    def set_garage(self, garage):
+        self.garage = garage
+
+    def set_robot(self, robot):
+        self.robot = robot
+
+    def add_obstacle(self, obstacle):
+        self.obstacles.append(obstacle)
+
+    def visualize(self):
+        """
+        Visualize the map. This function is used for debugging and presenting purposes.
+        """
+        # draw a 2D map from above with a grid and the found objects using matplotlib
+
+        # create a figure
+        fig, ax = plt.subplots()
+
+        # set the title
+        ax.set_title('Map')
+
+        # set the x and y limits so the robot is in the center of the bottom of the map
+        robot_x = self.robot.world_coordinates[0]
+        robot_y = self.robot.world_coordinates[1]
+        ax.set_xlim(robot_x - self.dimensions[0] / 2, robot_x + self.dimensions[0] / 2)
+        ax.set_ylim(robot_y, robot_y + self.dimensions[1])
+
+        # set the x and y labels
+        ax.set_xlabel('x [m]')
+        ax.set_ylabel('y [m]')
+
+        # set the grid
+        ax.grid()
+
+        # draw the garage
+        if self.garage is not None:
+            ax.add_patch(Rectangle(self.garage.world_coordinates, self.garage.width, self.garage.height,
+                                   angle=self.garage.orientation, color=self.garage.color))
+
+        # draw the obstacles
+        for obstacle in self.obstacles:
+            # an obstacle is always a circle
+            ax.add_patch(plt.Circle(obstacle.world_coordinates, obstacle.radius, color=obstacle.color))
+
+        # draw the robot
+        if self.robot is not None:
+            ax.add_patch(plt.Circle(self.robot.world_coordinates, self.robot.radius, color=self.robot.color))
+
+        # show the plot
+        plt.show()
+
+        # close the plot
+        plt.close(fig)
+
 
 class Obstacle:
-    def __init__(self, color, contour):
+    def __init__(self, radius, height, color, contour, world_coordinates=None):
+        self.radius = radius
+        self.height = height
         self.color = color
         self.contour = contour
+        self.world_coordinates = world_coordinates
+
+    def set_world_coordinates(self, world_coordinates):
+        self.world_coordinates = world_coordinates
+
+
+class Garage:
+    def __init__(self, length, width, height, color):
+        self.length = length
+        self.width = width
+        self.height = height
+        self.color = color
+        self.contour = None
         self.world_coordinates = None
-        self.image_coordinates = None
+        self.orientation = None
+
+    def set_contour(self, contour):
+        self.contour = contour
+
+    def set_orientation(self, orientation):
+        self.orientation = orientation
+
+    def set_world_coordinates(self, world_coordinates):
+        self.world_coordinates = world_coordinates
 
 
+class Gate:
+    def __init__(self, width, height, color, contour, world_coordinates=None, orientation=None):
+        self.width = width
+        self.height = height
+        self.color = color
+        self.contour = contour
+        self.world_coordinates = world_coordinates
+        self.orientation = orientation
+
+    def set_orientation(self, orientation):
+        self.orientation = orientation
+
+    def set_world_coordinates(self, world_coordinates):
+        self.world_coordinates = world_coordinates
+
+
+class Robot:
+    def __init__(self, radius, height, color):
+        self.radius = radius
+        self.height = height
+        self.color = color
+        self.world_coordinates = None
+
+    def set_world_coordinates(self, world_coordinates):
+        self.world_coordinates = world_coordinates
+
+
+if __name__ == '__main__':
+    # create a map
+    map = Map((5, 5))
+
+    # create a garage
+    garage = Garage(0.6, 0.5, 0.4, 'yellow', 'magenta')
+    garage.set_world_coordinates((-1, 3))
+    garage.set_orientation(32.5)
+
+    # create a robot
+    robot = Robot(0.2, 0.4, 'black')
+    robot.set_world_coordinates((0, 0))
+
+    # create multiple obstacles
+    obstacle = Obstacle(0.05, 0.4, 'red')
+    obstacle.set_world_coordinates((1, 3))
+
+    obstacle2 = Obstacle(0.05, 0.4, 'blue')
+    obstacle2.set_world_coordinates((-0.5, 2.5))
+
+    obstacle3 = Obstacle(0.05, 0.4, 'green')
+    obstacle3.set_world_coordinates((1, 1))
+
+    # add the objects to the map
+    map.set_garage(garage)
+    map.set_robot(robot)
+    map.add_obstacle(obstacle)
+    map.add_obstacle(obstacle2)
+    map.add_obstacle(obstacle3)
+
+    # visualize the map
+    map.visualize()
 
--- src/scripts/detection.py
+++ src/scripts/detection.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2 as cv
 import yaml
 
+from classes import Map, Obstacle, Garage, Gate
+
 
 def get_parking_spot_orientation(img: np.ndarray, piles: np.ndarray, detection_cfg: dict) -> None:
     """
@@ -19,79 +21,135 @@ def get_parking_spot_orientation(img: np.ndarray, piles: np.ndarray, detection_c
         # TODO
 
 
-def segmentation(img: np.ndarray, config: dict):
+def find_bounding_rects(img: np.ndarray, contours: list, min_area: int) -> list:
+    """
+    Find the bounding rectangles of the contours.
+    :param img: The image to draw bounding boxes on.
+    :param contours: The contours to process.
+    :param min_area: The minimum required area of the contour.
+    :return: A list of the bounding rectangles.
+    """
+    bounding_rects = []
+    for cnt in contours:
+        if cv.contourArea(cnt) < min_area:
+            continue
+        rect = cv.minAreaRect(cnt)
+        bounding_rects.append(rect)
+        box = cv.boxPoints(rect)
+        box = np.int0(box)
+        cv.drawContours(img, [box], 0, (0, 0, 255), 1)
+    return bounding_rects
+
+
+def segmentation(img: np.ndarray, map: Map, detection_cfg: dict, objects_cfg: dict) -> None:
     """
     Find objects of interest in the image. Distinguish between objects of interest in the foreground and background.
     """
     # Convert to HSV
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-    for color in config['colors'].values():
+    for color in detection_cfg['colors'].values():
         # Threshold the HSV image to get only the selected color
         lower = np.array(color['lower'])
         upper = np.array(color['upper'])
         mask = cv.inRange(hsv, lower, upper)
         output = cv.bitwise_and(img, img, mask=mask)
 
-        # # Get rid of noise
-        # kernel = np.ones((5, 5), np.uint8)
-        # output = cv.morphologyEx(output, cv.MORPH_OPEN, kernel)
+        # Get rid of noise
+        kernel = np.ones((5, 5), np.uint8)
+        output = cv.morphologyEx(output, cv.MORPH_OPEN, kernel)
 
         # Make the image binary
         output = cv.cvtColor(output, cv.COLOR_BGR2GRAY)
 
         # Find contours
-        contours, hierarchy = cv.findContours(output, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv.findContours(output, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
         # Draw contours
         copy_img = img.copy()
         cv.drawContours(copy_img, contours, -1, (0, 255, 0), 1)
 
-        if color['color'] == 'red' or color['color'] == 'green' or color['color'] == 'blue':
+        # Get the smallest rectangle that contains the contour (not for the garage)
+        if color['tag'] != objects_cfg['garage']['color']:
+            bounding_rects = find_bounding_rects(copy_img, contours, detection_cfg['min_detected_area'])
+
+        # Process what has been found
+        if color['tag'] in objects_cfg['obstacle']['color']:
             # Obstacles
-            cv.imshow(color['color'], copy_img)
-        else if color['color'] == 'magenta':
-            # Entrance
-        else:
+            set_up_obstacles(bounding_rects, map, color['tag'], objects_cfg)
+        elif color['tag'] == objects_cfg['gate']['color']:
+            # Gate
+            set_up_gate(bounding_rects, map, color['tag'], objects_cfg)
+        elif color['tag'] == objects_cfg['garage']['color']:
             # Garage
+            print('Garage')
+        else:
+            # Unknown
+            print('ERROR: Unknown color')
+            exit(-1)
 
-        # Get the smallest rectangle that contains the contour
-        for cnt in contours:
-            if cv.contourArea(cnt) < 300 or color['color'] == 'yellow':
-                continue
-            rect = cv.minAreaRect(cnt)
-            box = cv.boxPoints(rect)
-            box = np.int0(box)
-            cv.drawContours(copy_img, [box], 0, (0, 0, 255), 1)
-
-        if color['color'] == 'magenta':
-            get_parking_spot_orientation(img, contours, config)
-
-        # Show the result
+        # Visualize the result
         output = cv.cvtColor(output, cv.COLOR_GRAY2BGR)
         final = np.concatenate((copy_img, output), axis=1)
-        cv.imshow(color['color'], final)
+        cv.imshow(color['tag'], final)
         cv.waitKey(0)
 
 
-def process_image(img: np.ndarray) -> None:
+def set_up_gate(contours: list, map: Map, color: str, objects_cfg: dict) -> None:
+    """
+    Create Gate object for the found gate of certain color and add it to the map.
+    :param contours: The contours (in this case simplified by the bounding box of the actual contour) of slopes
+                    of the gate.
+    :param map: The map object.
+    :param objects_cfg: The 'objects config'.
+    :return: None
+    """
+    for contour in contours:
+        # Create the gate object
+        cf_gate = objects_cfg['gate']
+        gate = Gate(cf_gate['width'], cf_gate['height'], color, contour)
+        # Add the gate to the map
+        map.add_gate(gate)
+
+
+def set_up_obstacles(contours: list, map: Map, color: str, objects_cfg: dict) -> None:
+    """
+    Create Obstacle objects for the found obstacles of certain color and add them to the map.
+    :param contours: The contours (in this case simplified by the bounding box of the actual contour) of the obstacles.
+    :param map: The map object.
+    :param objects_cfg: The 'objects config'.
+    :return: None
+    """
+    for contour in contours:
+        # Create the obstacle object
+        cf_obstacle = objects_cfg['obstacle']
+        obstacle = Obstacle(cf_obstacle['radius'], cf_obstacle['height'], color, contour)
+        # Add the obstacle to the map
+        map.add_obstacle(obstacle)
+
+
+def process_image(img: np.ndarray) -> Map:
     """
     Process the image and show the result.
     :param img: The image to process.
     :return: None
     """
-    # Load config
+    # Load configs
     detection_cfg = yaml.safe_load(open('conf/detection.yaml', 'r'))
+    objects_cfg = yaml.safe_load(open('conf/objects.yaml', 'r'))
+
+    # Create the map object
+    map = Map(detection_cfg['map']['dimensions'])
 
     # Find objects of interest
-    segmentation(img, detection_cfg)
+    segmentation(img, map, detection_cfg, objects_cfg)
 
 
 if __name__ == '__main__':
-    for i in range(9):
-        if i == False:
+    for i in range(16):
+        if i == 3:
             continue
-        img = cv.imread(f'camera/shoot2/{i}.png')
+        img = cv.imread(f'camera/shoot1/{i}.png')
         process_image(img)
         cv.waitKey(0)
         cv.destroyAllWindows()
