c47f52c64b221efd0c21fe19024f68137038c7e4
Pepam01 <jmiv004@gmail.com>
2023-03-19 15:51:49 +0100

    movements functionalities added. Straight and rotation Ok

--- src/scripts/move.py
+++ src/scripts/move.py
@@ -6,63 +6,112 @@ import yaml
 
 
 class move:
-    move_coords = list(list())
-    
+    move_coords = list(list())  #2D array of moving coords
 
     def __init__(self, coords):
         self.detection_cfg = yaml.safe_load(open('conf/detection.yaml', 'r'))
         self.move_coords = coords
         self.turtle = Turtlebot()
-        self.rate = Rate(10)
+        self.rate = Rate(10) # co dela tahle funkce?
 
-    def rotate_degrees(self, degrees):
-        self.turtle.reset_odometry()
-        temp = self.turtle.get_odometry()[2]
-        goal = np.degrees(temp) + degrees
-        goal_radians = np.radians(goal)
+    def execute_move(self):
+        # d
+        pass
 
-        radians = np.radians(degrees) 
-        print(goal_radians)
-        while not self.turtle.is_shutting_down() :
-            if(degrees < 0):
-                self.turtle.cmd_velocity(angular = -0.5)
-            if(degrees > 0):
-                self.turtle.cmd_velocity(angular = 0.5)
-
-            actual_rotation = self.turtle.get_odometry()[2]
-            print(goal_radians, actual_rotation)
-            self.rate.sleep()
-
-    def test(self):
-        t = get_time()
-
-        '''while get_time() - t < 10:
-            self.turtle.cmd_velocity(linear=0.1)
-            self.rate.sleep()
-'''
-        #self.turtle.reset_odometry()
-        '''t = get_time()
+    #exports nonlinear path, to linear
+    def non_to_linear_path(self, non_linear_path):
+        linear_path = list()
+        return linear_path
+    
+    # length in centimeters
+    def go_straight(self, length, dir):
+        slow_start_cnt = 0
+        slowdown = False
+        odometry_x_y = []
+        #path_integrated = list()
+        self.odometry_hard_rst()
         while not self.turtle.is_shutting_down():
-            if get_time() - t > 0.5*6.28:
+            #print(slow_start_cnt)
+            if(slow_start_cnt < 1.0 and not slowdown):
+                slow_start_cnt = slow_start_cnt + 0.05
+            elif(slowdown and slow_start_cnt >= 0.17):
+                slow_start_cnt = slow_start_cnt - 0.05
+
+            odometry_x_y = self.turtle.get_odometry()[:1]
+            if(abs(odometry_x_y[0]) >= (length/100)):
                 break
-            self.turtle.cmd_velocity(angular = -2)
-            self.rate.sleep() 
-'''
-        self.turtle.reset_odometry()
-        while not self.turtle.is_shutting_down():
-            linear_motion = list() # bude to list linearnich motionu
-            var = self.turtle.get_odometry()
-            #if (var[0] >= 1 or var[1] >= 1):
-            #    break
-            print(var[0], var[1], var[2])
-            #direction = np.sign(np.random.rand() - 0.5)
-            self.turtle.cmd_velocity(angular=-0.3)
-            #turtle.cmd_velocity(linear=0.1)
+            self.turtle.cmd_velocity(linear=(0.2*dir)*slow_start_cnt) # TODO add as yaml const
             self.rate.sleep()
+            print((abs(odometry_x_y[0])/2), ((length/100)/2))
+            if((abs(odometry_x_y[0])) > ((length/100)/1.3)):
+                slowdown = True
+        #path_integrated.append(odometry_x_y[0])
+        #path_integrated.append(odometry_x_y[1])
+        return list(odometry_x_y[0], odometry_x_y[1])
+
+    def compensate_straight_integration_drift(self):
+        additional_rotation = 0
+        #pricitej zataceci error podle integrace k jiz planovanemu zatoceni
+        return additional_rotation
+
+    def odometry_hard_rst(self):
+        self.turtle.reset_odometry()
+        self.turtle.wait_for_odometry()
+        self.turtle.reset_odometry()
+
+    # this is compensation for error drift after rotation
+    def compensation(self, direction):
+        #TODO move compensation constant to yaml
+
+        self.odometry_hard_rst()
+        if(direction == -1):
+            while not self.turtle.is_shutting_down():
+                self.turtle.cmd_velocity(angular = 0.4)
+                act_rot = self.turtle.get_odometry()[2]
+                print(act_rot)
+                if(act_rot >= 0.08):
+                    break
+
+        elif(direction == 1):
+            while not self.turtle.is_shutting_down():
+                self.turtle.cmd_velocity(angular = -0.4)
+                act_rot = self.turtle.get_odometry()[2]
+                print(act_rot)
+                if(act_rot <= -0.08):
+                    break
+                
+    # degree input range have to be +-180 deg
+    def rotate_degrees(self, degrees):
+        self.odometry_hard_rst()
+
+        if(degrees > 0):
+            prev_rot = 0
+            goal = np.radians(degrees)
+
+            while not self.turtle.is_shutting_down():
+                act_rot = self.turtle.get_odometry()[2]
+                print(act_rot, prev_rot)
+                if(act_rot >= goal or abs(act_rot-prev_rot) > 1):
+                    break
+                self.turtle.cmd_velocity(angular = 0.4)
+                prev_rot = act_rot
+            self.compensation(1)
+  
+        elif(degrees < 0):
+            prev_rot = 0
+            goal = np.radians(degrees)
+
+            while not self.turtle.is_shutting_down():
+                act_rot = self.turtle.get_odometry()[2]
+                print(act_rot, prev_rot)
+                if(act_rot <= goal or abs(act_rot - prev_rot) > 1):
+                    break
+                self.turtle.cmd_velocity(angular = -0.4)
+                prev_rot = act_rot
+            self.compensation(-1)
         
 
 tmp = move(None)
-tmp.rotate_degrees(90)
-for i in range(0):
-    tmp.rotate_degrees(45)
-#tmp.test()
+#tmp.rotate_degrees(-60)
+tmp.go_straight(100,1)
+#tmp.rotate_degrees(-180)
