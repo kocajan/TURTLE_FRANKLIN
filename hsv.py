5743ce1bac824d031004d29fade64764b5735cf7
Jan Koca <kocajan@bullseye>
2023-03-23 15:28:05 +0100

    detection

--- hsv.py
+++ hsv.py
@@ -25,7 +25,7 @@ cv2.setTrackbarPos('VMax', 'image', 255)
 hMin = sMin = vMin = hMax = sMax = vMax = 0
 phMin = psMin = pvMin = phMax = psMax = pvMax = 0
 
-img = cv2.imread('src/scripts/camera/shoot2/5.png')
+img = cv2.imread('src/scripts/camera/shoot5/RGB0.png')
 img = cv2.resize(img, (670, 502))
 output = img
 waitTime = 33
