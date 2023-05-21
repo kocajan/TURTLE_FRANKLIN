2434fc4956c44517e2e4a77d74ce8753e88fa2e1
Jan Koca <kocajan@bullseye>
2023-05-21 08:39:50 +0200

    idk

--- src/path_genesis.py
+++ src/path_genesis.py
@@ -14,7 +14,7 @@ for i in range(500):
     if i > 400 and i <= 500:
         k = i -400
         path1.append((250,200+k))
-print(path1)
+#print(path1)
 
 path2 = [] #rovne sikmo 45 sikm -45 (90) rovne
 for i in range(500):
@@ -28,8 +28,25 @@ for i in range(500):
         path2.append((350,200+k))
     if i > 300 and i <= 400:
         k = i -300
-        path2.append((350-k,200-k))
+        path2.append((350-k,300-k))
     if i > 400 and i <= 500:
         k = i -400
-        path2.append((250,100+k))
+        path2.append((250,200+k))
+#print(path2)
+path2 = [] #rovne sikmo 45 sikm -45 (90) rovne //shorter
+for i in range(50):
+    if i <=10:
+        path2.append((250,i)) #rovne
+    if i > 10 and i <= 20:
+        k = i -10
+        path2.append((250+k,10+k))
+    if i > 20 and i <= 30:
+        k = i -20
+        path2.append((260,20+k))
+    if i > 30 and i <= 40:
+        k = i -30
+        path2.append((260-k,30-k))
+    if i > 40 and i <= 50:
+        k = i -40
+        path2.append((250,20+k))
 print(path2)
